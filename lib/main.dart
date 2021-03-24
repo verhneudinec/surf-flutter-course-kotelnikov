1140c7fcda4f1e65c14cf889b2e437b68ef1b7a4
Igor` <webarts@yandex.ru>
2021-03-25 03:30:59 +0800

    Working with redux architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:places/data/repository/place_repository.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:places/data/model/app_state.dart';
+import 'package:places/data/redux/middleware/places_search_middleware.dart';
+import 'package:places/data/repository/filtered_places_repository.dart';
 import 'package:places/data/store/places_store/places_store.dart';
-import 'package:places/ui/screen/favorites_bloc_demo_screen.dart';
+import 'package:places/ui/screen/search_screen_redux_demo.dart';
 import 'package:provider/provider.dart';
-
 import 'package:places/ui/view_model/add_place_model.dart';
 import 'package:places/res/app_routes.dart';
 import 'package:places/res/themes.dart';
 import 'package:places/ui/screen/map_screen.dart';
 import 'package:places/ui/screen/places_list_screen.dart';
-import 'package:places/ui/screen/splash_screen.dart';
 import 'package:places/ui/screen/favorites_screen.dart';
 import 'package:places/ui/screen/settings_screen.dart';
 import 'package:places/ui/view_model/places_search_model.dart';
@@ -17,8 +18,20 @@ import 'package:places/data/interactor/settings_interactor.dart';
 import 'package:places/ui/view_model/place_types_model.dart';
 import 'package:places/data/interactor/places_interactor.dart';
 import 'package:places/data/interactor/places_search_interactor.dart';
+import 'package:redux/redux.dart';
+import 'data/redux/reducer/places_search_reducer.dart';
 
 void main() {
+  final store = Store<AppState>(
+    reducer,
+    initialState: AppState(),
+    middleware: [
+      SearchMiddleware(
+        FilteredPlaceRepository(),
+      ),
+    ],
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -35,12 +48,15 @@ void main() {
         /// For MobX demo
         Provider(create: (_) => PlacesStore()),
       ],
-      child: App(),
+      child: App(store: store),
     ),
   );
 }
 
 class App extends StatefulWidget {
+  final Store<AppState> store;
+
+  const App({Key key, this.store}) : super(key: key);
   @override
   _AppState createState() => _AppState();
 }
@@ -52,16 +68,19 @@ class _AppState extends State<App> {
     bool _isDarkMode = context.watch<SettingsInteractor>().isDarkMode;
 
     /// Run the application
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: _isDarkMode ? darkTheme : lightTheme,
-      home: FavoritesBlockDemoScreen(),
-      routes: {
-        AppRoutes.home: (BuildContext context) => PlaceListScreen(),
-        AppRoutes.map: (BuildContext context) => MapScreen(),
-        AppRoutes.favorites: (BuildContext context) => FavoritesScreen(),
-        AppRoutes.settings: (BuildContext context) => SettingsScreen(),
-      },
+    return StoreProvider<AppState>(
+      store: widget.store,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: _isDarkMode ? darkTheme : lightTheme,
+        home: PlaceSearchScreenReduxDemo(),
+        routes: {
+          AppRoutes.home: (BuildContext context) => PlaceListScreen(),
+          AppRoutes.map: (BuildContext context) => MapScreen(),
+          AppRoutes.favorites: (BuildContext context) => FavoritesScreen(),
+          AppRoutes.settings: (BuildContext context) => SettingsScreen(),
+        },
+      ),
     );
   }
 }
